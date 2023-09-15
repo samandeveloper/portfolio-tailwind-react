ec0549e7972efd8ebed6108d01459eb8d131f782
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2023-09-15 11:38:56 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import { useState } from "react";
 
 const App = () => {
+  //states
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <>
-      <Navbar />
-      <Hero />
+    <main className={toggle ? "bg-black" : ""}>
+      <Navbar toggle={toggle} handleClick={handleClick} />
+      <Hero toggle={toggle} />
       <Skills />
-      <About />
+      <About toggle={toggle} />
       <Projects />
-    </>
+    </main>
   );
 };
 export default App;
